d83a39758d83d2583f8198c5f7c4a19fefdb99ee
alfredo777 <jose_alfredo@cursa.me>
2014-08-14 17:54:26 -0500

    cambios en ventas

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -11,8 +11,8 @@ skip_before_filter :verify_authenticity_token
 	  end
     
 
-    	ida = params[:ascesor]
-
+    	ida = params[:seller]
+      puts "procesando pagos por el vendedor #{ida}"
 	    case params[:amount]
 	   		when 100000
 	   			id = 'plan_inicial'
@@ -27,7 +27,7 @@ skip_before_filter :verify_authenticity_token
 		 charge = Conekta::Charge.create({
 		      amount: params[:amount],
 		      currency: 'MXN',
-		      description: "payment suscription #{current_user.name} - #{current_user.id}",
+		      description: "payment suscription #{current_user.name} - #{current_user.id} --> vendedor #{ida}",
 		      card: params[:conektaTokenId],
 		      reference_id: "#{id}"
 		  })
@@ -37,12 +37,18 @@ skip_before_filter :verify_authenticity_token
           
 		rescue Conekta::ParameterValidationError => e
 		  puts e.message 
+		  flash[:notice] = "Error al intentar pagar algúno de los parámetros es invalido"
+		  redirect_to :back
 		#alguno de los parámetros fueron inválidos
 		rescue Conekta::ProcessingError => e
 		  puts e.message 
+		  flash[:notice] = "Lo sentimos la tarjeta no pudo ser procesada"
+		  redirect_to :back
 		#la tarjeta no pudo ser procesada
 		rescue Conekta::Error
 		  puts e.message 
+		  flash[:notice] = "Lo sentimos el sistema de cobros ha tenido un error inesperado y no ha procesado su pago"
+		  redirect_to :back
 		#un error ocurrió que no sucede en el flujo normal de cobros como por ejemplo un auth_key incorrecto
 		end
 		respond_to do |format|
