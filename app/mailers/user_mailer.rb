8bcaa7f5d1dbc82e69fd575009e7f025cfda8fb6
alfredo777 <jose_alfredo@cursa.me>
2014-09-22 18:07:36 -0500

    cmabios en direcci´n de contacto

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -35,6 +35,6 @@ class UserMailer < ActionMailer::Base
      @email = email
      @name = name
      @description = description
-     mail(to: 'jardarubydv@gmail.com', subject: "La persona #{@name} se ha puesto en contacto contigo.")
+     mail(to: 'idoctorecontacto@gmail.com', subject: "La persona #{@name} se ha puesto en contacto contigo.")
    end
 end
