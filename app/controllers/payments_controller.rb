9089932004a4dd5af9669c781c173d7077d7e876
alfredo777 <jose_alfredo@cursa.me>
2014-11-09 20:43:14 -0600

    cambio en error

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -122,9 +122,9 @@ class PaymentsController < ApplicationController
 	    @p = Payment.create(user_id: session[:paymenttouser].to_i, payment_global: @global.to_i, bank_commission: @comission, final_comission: @comission_seller, init: Time.now, expire: @expire, comissionpay: false, seller_code: params[:seller], method: 'Cash', token_pay: 'no token')
 	    if @p.save
 	    	 puts session[:paymenttouser]
-             session[:paymenttouser] = nil
-             @user = User.find(session[:paymenttouser])
+	    	 @user = User.find(session[:paymenttouser])
              @user.update_attributes(payment_method: true)
+             session[:paymenttouser] = nil
              flash[:notice] = 'Pago realizado'
 		     redirect_to seller_path
 	    else
