402a1b284e5fe0ee03c4632d535106b322c1d6f6
alfredo777 <jose_alfredo@cursa.me>
2014-11-24 13:05:48 -0600

    cmabios en geocode

--- app/controllers/static_views_controller.rb
+++ app/controllers/static_views_controller.rb
@@ -14,9 +14,6 @@ class StaticViewsController < ApplicationController
   end
 
   def prices
-    if Rails.env == 'development'
-    @location = Location.create(ip_address: '189.209.186.179')
-    else    
     @location = Location.create(ip_address: request.remote_ip)
     puts @location.ubicate_in
     end 
