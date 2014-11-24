fd1c205ea12bf7d4ad4c23da0d38548ee2a9b4cc
alfredo777 <jose_alfredo@cursa.me>
2014-11-24 17:03:51 -0600

    data finder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,10 +37,14 @@ function getLocation() {
 
 function showPosition(position) {
     $.get( '/static_views/create_location', { latitude: position.coords.latitude, longitude: position.coords.longitude }, function( data ) {
-     var findToAccesString = ['Acapulco', 'Guerrero', 'Mexico'] 
+     var findToAccesString = ['Acapulco', 'Guerrero', 'Mexico'. 'Federal District'] 
      console.log(data);
        $.each(data, function(i, item) {
-         console.log(item.data.formatted_address);
+        $.each(findToAccesString, function(i, wordtofind) {
+         var str = item.data.formatted_address
+         var n = str.search(wordtofind);
+          console.log(n);
+        }
       });
 
     });
