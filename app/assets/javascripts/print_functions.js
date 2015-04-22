91d2b27e3d0fb69802608d2fb05ecb4001cadbfb
alfredo777 <jose_alfredo@cursa.me>
2015-04-22 18:29:31 -0500

    cambios en funciones

--- app/assets/javascripts/print_functions.js
+++ app/assets/javascripts/print_functions.js
@@ -6,17 +6,9 @@
 	     window.location = url
  }
 
- function printTherapy(qr, treatments, user_id, general_d, user_name, user_token){
- 	 	 /*var url = "/users/"+user_id+"/diagnostics"
-         var qr_c =  $(qr).html();
-         var general_d_c = $(general_d).html();
-         var treatments_c =  $(treatments).html();
-         var user_name_c = $(user_name).html();
-         var user_token_c = $(user_token).html();
- 	     var content =  general_d_c + treatments_c ;*/
+ function printTherapy(user_id){
+         var url = "/users/"+user_id+"/diagnostics";
          var content = $('#print_x').html();
- 	     //var privacidad = 'Queda prohibido replicar, clonar o intercambiar esta receta. Esta receta esta registrada electrónicamente por IDoctore.'
- 	     // document.body.innerHTML = '<html><head><title>'+head_c+'</title></head><body><div style="width:100%; height:100px; font-size:30px; ">'+head_c+'</div>'+'<div style="margin-top:-90px;">'+address+'</div>'+content+'<div style=" background-color: #BDBDBD !important; color:#ffffff !important; padding:10px !important; margin-top:150px; -webkit-print-color-adjust: exact;"><i>'+privacidad+'</i></div> </body><footer style="background-color: #F3F3F3; padding-top: 10px; padding-bottom: 0px; position:fixed; bottom:0; width:100%;"><div style="width:80%; float:left;">'+ user_name_c+ '<p style="font-size:7px;">' + user_token_c + '</p></div><div style="float:left; width:20%;"><center>' + qr_c +'</center></div></footer></html>';
  	     document.body.innerHTML = content;
          window.print();
 	     window.location = url;
