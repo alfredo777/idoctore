a1cda81bd8c191559771919099d86eb0b5e22026
alfredo777 <jose_alfredo@cursa.me>
2015-12-03 13:28:16 -0600

    cambio en dropdown que fallaba

--- app/assets/javascripts/clinical_histories.js
+++ app/assets/javascripts/clinical_histories.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     var target = $(this).data('target');
     if ($('#'+target).css('display') == 'none'){
       $('.uppart').html('<h6 style="color:#ffffff;">Abrir <i class="fa fa-chevron-down"></i></h6>');
-      $('.body-ax').hide();
+      $(".body-ax").hide();
       $('#'+target).slideToggle('slow');
       $(this).html('<h6 style="color:#ffffff;">Cerrar <i class="fa fa-chevron-up"></i></h6>');
 
